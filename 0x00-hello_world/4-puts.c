244b527b57ed638dcdc9133ab9df552d25fe7711
SWAAFIYAH <swaafiyah9@gmail.com>
2023-06-15 16:07:10 -0700

    4th commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main -  programming is like building a multilingual puzzle
- * return : always 0(success)
+ * main - A programme that uses puts function to print a line
+ * Return 0(success)
  */
 int main(void)
 {
-	puts(*\"programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
- 
