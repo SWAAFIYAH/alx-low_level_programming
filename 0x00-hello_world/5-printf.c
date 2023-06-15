d0dfc2d4f20a7d557dde235e2b5b80018f78f180
SWAAFIYAH <swaafiyah9@gmail.com>
2023-06-15 15:42:19 -0700

    5th task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A programme that uses a printf function to print a line
+ * Return: 0(success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
