dc92f4c5464ab94496fa92aff2a57aead67cc9fc
SWAAFIYAH <swaafiyah9@gmail.com>
2023-06-29 13:11:01 -0700

    task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,20 +1,19 @@
 #include "main.h"
 /**
  * string_toupper - change all lowercase to uppercase
- * @i: integer
- * Return: n
+ * @str: string to return
+ * Return: string
  */
-char *string_toupper(char *)
+char *string_toupper(char *str)
 {
 	int i;
 
-	i = 0;
-
-	while (n[i] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (n[i] >= 'a' && n[i] <= 'z')
-			n[i] = n[i - 32;
-		i++;
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
 	}
-	return (n);
+	return (str);
 }
