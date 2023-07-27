07141bbc79debf7397a89e7724403ff358f2649c
SWAAFIYAH <swaafiyah9@gmail.com>
2023-07-27 11:23:17 -0700

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,22 +1,25 @@
-#include "lists.h"
 #include <stdio.h>
+#include "lists.h"
+
 /**
- * print_list - prints all elements of linked list
- * @h: pointer to the list to print
+ * print_list - prints  elements of  linked list
+ * @h: pointer to the list_t list to be printed
  * Return: no. of nodes printed
  */
 size_t print_list(const list_t *h)
 {
-	size_t x = 0;
+size_t s = 0;
 
-	while (h)
-	{
-		if (!h->str)
-			printf("|0| (nil)\n");
-		else
-			printf("|%u| %s\n", h->len, h->str);
-		h = h->next;
-		x++;
-	}
-	return (x);
+while (h)
+{
+if (!h->str)
+printf("[0] (nil)\n");
+else
+printf("[%u] %s\n", h->len, h->str);
+h = h->next;
+s++;
 }
+
+return (s);
+}
+
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,14 +1,14 @@
 #ifndef LISTS_H
 #define LISTS_H
-#include <stddef.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
  *
- * Description: singly linked list node structure for
- * Holberton project
+ * Description: singly linked list node structure
+ * for Holberton project
  */
 typedef struct list_s
 {
@@ -24,3 +24,4 @@ list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
 #endif
+
