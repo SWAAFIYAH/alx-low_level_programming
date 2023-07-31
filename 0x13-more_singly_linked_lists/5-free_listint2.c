616a509e91e8b51b6bcccbc4794f1bf4fa1f39a8
SWAAFIYAH <swaafiyah9@gmail.com>
2023-07-31 11:51:47 -0700

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees linked list
+ * @head: head of linked list
+ * sets head to NULL
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *m;
+
+	if (head == NULL)
+		return;
+	while (*head)
+	{
+		m = (*head)->next;
+		free(*head);
+		*head = m;
+	}
+	head = NULL;
+}
